858bc9018c92f4469d986168abc69e71cfa36de3
BenTenmann <benji.tenmann@me.com>
2021-11-28 14:46:35 +0000

    feat: expose TcrDist to parent package

--- setriq/modules/__init__.py
+++ setriq/modules/__init__.py
@@ -7,4 +7,5 @@ __all__ = [
     'BLOSUM62',
     'BLOSUM90',
     'CdrDist',
+    'TcrDist',
 ]
--- setriq/modules/_distances.py
+++ setriq/modules/_distances.py
@@ -10,6 +10,8 @@ from ._substitution import *
 
 __all__ = [
     'CdrDist',
+    'TcrDist',
+    'TcrDistComponent',
 ]
 
 
