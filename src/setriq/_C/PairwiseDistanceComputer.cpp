e9daf7b1eb1cb252d7c0aacd6b95f98bee2f3f5a
BenTenmann <benji.tenmann@me.com>
2021-12-03 22:48:47 +0000

    chore: remove std::cout from pragma

--- src/setriq/_C/PairwiseDistanceComputer.cpp
+++ src/setriq/_C/PairwiseDistanceComputer.cpp
@@ -12,7 +12,7 @@ doubleVector PairwiseDistanceComputer::computeDistance(const stringVector& input
     size_t N = inputStrings.size();
     doubleVector distanceMatrix(N * (N - 1) / 2);
 
-    #pragma omp parallel for default(none) shared(inputStrings, distanceMatrix, N, std::cout)
+    #pragma omp parallel for default(none) shared(inputStrings, distanceMatrix, N)
     for (size_t i = 0; i < N - 1; i++) {
         for (size_t j = i + 1; j < N; j++) {
             size_t index = (N * (N - 1)) / 2 - (N - i) * ((N - i) - 1) / 2 + j - i - 1;
